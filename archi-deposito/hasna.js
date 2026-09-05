a2f1ec90155ac6909508cbab0ce2067e1eb3916e
bimtolproject <bimtol2502@gmail.com>
2026-09-05 15:27:12 +0700

    Update Saldo: Rp 1

--- archi-deposito/hasna.js
+++ archi-deposito/hasna.js
@@ -1,110 +1,106 @@
 const hasnaData = {
-    "version": 1.0,
+    "version": 1.01,
     "user": {
         "name": "Arch",
         "accountType": "Black Diamond Tier",
         "accountNumber": "2500 0012 772"
     },
-    
-    // Hanya kantong utama (saldo murni dibaca dari field balance)
     "kantongUtama": {
         "id": "k_utama",
         "name": "Rekening Utama",
-        "balance": 2750000,
+        "balance": 1,
         "currency": "IDR"
     },
-
-    // Riwayat transaksi murni
     "riwayat": [
         {
-            id: "t71", 
-            title: "Biaya Transfer + Admin", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 3000, 
-            date: "02 Sep 2026 - 18:00", 
-            icon: "fa-minus" 
+            "id": "t71",
+            "title": "Biaya Transfer + Admin",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 3000,
+            "date": "02 Sep 2026 - 18:00",
+            "icon": "fa-minus"
         },
         {
-            id: "t70", 
-            title: "Transfer Dana", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 8365, 
-            date: "02 Sep 2026 - 18:00", 
-            icon: "fa-arrow-up" 
+            "id": "t70",
+            "title": "Transfer Dana",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 8365,
+            "date": "02 Sep 2026 - 18:00",
+            "icon": "fa-arrow-up"
         },
         {
-            id: "t67", 
-            title: "Biaya Transfer + Admin", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 3000, 
-            date: "02 Sep 2026 - 18:00", 
-            icon: "fa-minus" 
+            "id": "t67",
+            "title": "Biaya Transfer + Admin",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 3000,
+            "date": "02 Sep 2026 - 18:00",
+            "icon": "fa-minus"
         },
         {
-            id: "t66", 
-            title: "Transfer Dana", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 25000, 
-            date: "02 Sep 2026 - 18:00", 
-            icon: "fa-arrow-up" 
+            "id": "t66",
+            "title": "Transfer Dana",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 25000,
+            "date": "02 Sep 2026 - 18:00",
+            "icon": "fa-arrow-up"
         },
         {
-            id: "t64", 
-            title: "Transfer Bank Jago", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 55000, 
-            date: "02 Sep 2026 - 12:00", 
-            icon: "fa-arrow-up" 
+            "id": "t64",
+            "title": "Transfer Bank Jago",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 55000,
+            "date": "02 Sep 2026 - 12:00",
+            "icon": "fa-arrow-up"
         },
         {
-            id: "t63", 
-            title: "Deposito Via Transfer", 
-            pocket: "Rekening Utama", 
-            type: "in", 
-            amount: 100000, 
-            date: "01 Sep 2026 - 18:31", 
-            icon: "fa-plus" 
+            "id": "t63",
+            "title": "Deposito Via Transfer",
+            "pocket": "Rekening Utama",
+            "type": "in",
+            "amount": 100000,
+            "date": "01 Sep 2026 - 18:31",
+            "icon": "fa-plus"
         },
         {
-            id: "t62", 
-            title: "Transfer Dana", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 22835, 
-            date: "01 Sep 2026 - 00:00", 
-            icon: "fa-arrow-up" 
+            "id": "t62",
+            "title": "Transfer Dana",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 22835,
+            "date": "01 Sep 2026 - 00:00",
+            "icon": "fa-arrow-up"
         },
         {
-            id: "t58", 
-            title: "Deposito Via QRIS", 
-            pocket: "Rekening Utama", 
-            type: "in", 
-            amount: 150000, 
-            date: "30 Agu 2026 - 16:00", 
-            icon: "fa-plus" 
+            "id": "t58",
+            "title": "Deposito Via QRIS",
+            "pocket": "Rekening Utama",
+            "type": "in",
+            "amount": 150000,
+            "date": "30 Agu 2026 - 16:00",
+            "icon": "fa-plus"
         },
         {
-            id: "t55", 
-            title: "QRIS Ke PT Tokopedia", 
-            pocket: "Rekening Utama", 
-            type: "out", 
-            amount: 117862, 
-            date: "22 Agu 2026 - 00:17", 
-            icon: "fa-arrow-up" 
+            "id": "t55",
+            "title": "QRIS Ke PT Tokopedia",
+            "pocket": "Rekening Utama",
+            "type": "out",
+            "amount": 117862,
+            "date": "22 Agu 2026 - 00:17",
+            "icon": "fa-arrow-up"
         },
         {
-            id: "t54", 
-            title: "Deposito Via QRIS", 
-            pocket: "Rekening Utama", 
-            type: "in", 
-            amount: 202000, 
-            date: "21 Agu 2026 - 07:00", 
-            icon: "fa-plus" 
+            "id": "t54",
+            "title": "Deposito Via QRIS",
+            "pocket": "Rekening Utama",
+            "type": "in",
+            "amount": 202000,
+            "date": "21 Agu 2026 - 07:00",
+            "icon": "fa-plus"
         }
     ]
 };
